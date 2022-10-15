131cd46aa2149b3fef8a1e3b408135756e2731c9
taka <taka@DESKTOP-1FGC2TI.localdomain>
2022-10-15 15:37:42 +0900

    event store

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -25,13 +25,13 @@
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
                 <p class="text-sm text-gray-700 leading-5">
-                    {!! __('Showing') !!}
+                    表示
                     @if ($paginator->firstItem())
-                        <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                        {!! __('to') !!}
-                        <span class="font-medium">{{ $paginator->lastItem() }}</span>
+                    <span class="font-medium">{{ $paginator->firstItem() }}</span>
+                    {!! __('to') !!}
+                    <span class="font-medium">{{ $paginator->lastItem() }}</span>
                     @else
-                        {{ $paginator->count() }}
+                    {{ $paginator->count() }}
                     @endif
                     {!! __('of') !!}
                     <span class="font-medium">{{ $paginator->total() }}</span>
